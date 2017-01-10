8eff25a3605b049f375b96a79840261c2d2a3402
罄天 <qinliang.ql@alibaba-inc.com>
2017-01-10 18:05:38 +0800

    添加注释

--- src/plugins/serve-index.js
+++ src/plugins/serve-index.js
@@ -1,5 +1,6 @@
 
 export default {
+  //这里是'middleware'阶段
   middleware() {
     return require('koa-serve-index')(this.cwd, {
       hidden: true,
--- src/resolve.js
+++ src/resolve.js
@@ -10,6 +10,16 @@ function tryResolve(id, dirname) {
   return result;
 }
 
+/*
+ (1)调用方式如下：
+    resolve(pluginName, resolveDir)
+ (2)它会在指定的目录下寻找dora-plugin-name和name插件，如果有那么直接返回
+ (3)我们的resolve会从指定的目录开始查找：
+    result = resolve.sync(id, {
+      basedir: dirname,
+    });
+    默认是查找node_modules目录
+*/
 export default function(id, _resolveDir) {
   let resolveDir = _resolveDir;
   if (!Array.isArray(resolveDir)) {
